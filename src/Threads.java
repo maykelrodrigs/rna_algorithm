1a80c4778b462a65ce5252b7036d3b7ad5582b6e
Maykel Rodrigues <maykelrodrigues@live.com>
2017-04-19 18:57:43 -0300

    Testando o multicast

---
+++ src/Threads.java
@@ -0,0 +1,35 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mrodrigues
+ */
+public class Threads extends Thread {
+    
+    Servidor s;
+
+    public Threads(Servidor s) {
+        this.s = s;
+    }
+    
+    @Override
+    public void run() {
+     
+        try {
+            
+            s.receberTCP();
+            Thread.sleep(500);
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Threads.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+}
